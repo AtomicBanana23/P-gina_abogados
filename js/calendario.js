841bd03fd0d652d8ffd3c5d2f65d5b306a78da78
Scarkat <119711013+Scarkat@users.noreply.github.com>
2024-05-15 23:04:33 -0600

    tumami
    me quiero comer una pastilla de cianuro

--- js/calendario.js
+++ js/calendario.js
@@ -1,7 +1,12 @@
+    // Inicializar el datepicker
+    $('.cl-date').datepicker({
+        format: "dd/mm/yyyy",
+        daysOfWeekDisabled: "0,6",
+        daysOfWeekHighlighted: "1,2,3,4,5",
+        todayBtn: "linked"
+    });
 
-$('.cl-date').datepicker({
-    format: "dd/mm/yyyy",
-    daysOfWeekDisabled: "0,6",
-    daysOfWeekHighlighted: "1,2,3,4,5"
-});
-
+    $('#save-date-btn').on('click', function() {
+        // Obtener el valor del datepicker
+        var selectedDate = document.getElementById("datepicker-input").value;
+    });
